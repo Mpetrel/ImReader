0c9aeceec284d961733afe87facb69a6e034c5ba
mpetrel <mmpetrel@live.com>
2019-04-26 01:06:34 +0800

    finish search view style

--- app/src/main/java/im/dule/imreader/MainActivity.kt
+++ app/src/main/java/im/dule/imreader/MainActivity.kt
@@ -12,8 +12,8 @@ import androidx.appcompat.widget.SearchView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val useThemestatusBarColor = false //是否使用特殊的标题栏背景颜色，android5.0以上可以设置状态栏背景色，如果不使用则使用透明色值
-    private val useStatusBarColor = true //是否使用状态栏文字和图标为暗色，如果状态栏采用了白色系，则需要使状态栏和图标为暗色，android6.0以上可以设置
+    private var searchAutoComplete: SearchView.SearchAutoComplete? = null
+    private var searchView1: SearchView? = null
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
@@ -40,16 +40,33 @@ class MainActivity : AppCompatActivity() {
 
         // 添加toolbar
         setSupportActionBar(this.toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(false)
+        supportActionBar!!.setHomeButtonEnabled(false)
         // 设置暗色状态栏按钮
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         title = "Books"
+
+        this.toolbar.setNavigationOnClickListener {
+            if (!searchView1!!.isIconified) {
+                closeSearch()
+            }
+        }
     }
 
+    /**
+     * create option menu,change searchview default action
+     */
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
 
         val searchItem = menu!!.findItem(R.id.search)
         val searchView = searchItem.actionView as SearchView
+        searchView1 = searchView
+        searchAutoComplete = searchView.findViewById(R.id.search_src_text) as SearchView.SearchAutoComplete
+        //设置搜索框有字时显示叉叉，无字时隐藏叉叉
+        searchView.onActionViewExpanded()
+        searchView.isIconified = true
+        searchView.queryHint = "Search Books..."
 
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -60,6 +77,11 @@ class MainActivity : AppCompatActivity() {
                 return false
             }
         })
+
+        searchView.setOnSearchClickListener {
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.setHomeButtonEnabled(true)
+        }
 
         return true
     }
@@ -75,6 +97,25 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+
+    override fun onBackPressed() {
+        if (!searchView1!!.isIconified) {
+            closeSearch()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
+    private fun closeSearch() {
+        searchView1!!.setQuery("", false)
+        searchView1!!.clearFocus()
+        searchView1!!.isIconified = true
+        supportActionBar!!.setDisplayHomeAsUpEnabled(false)
+        supportActionBar!!.setHomeButtonEnabled(false)
+    }
+
 
 
 }
